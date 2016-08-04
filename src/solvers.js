c4b1abed8dcc7df7b3693afe334355cd3d8d4dc2
Simeon Lee <simeonhlee@gmail.com>
2016-08-04 16:26:43 -0700

    Refactor code to more closely align with solution video

--- src/solvers.js
+++ src/solvers.js
@@ -13,152 +13,82 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
+window.findSolution = function (row, n, board, validator, callback) {
+
+  // If we reach the end successfully, push the solution to solutions array
+  if (row === n) {
+    return callback();
+  }
+
+  // Iterate over columns in the selected row
+  for ( var j = 0; j < n; j++ ) {
+
+    // Create a variant of the board with placement of piece at new col
+    // 'Row' variable is set at function invocation
+    board.togglePiece(row, j);
+
+    if ( !board[validator]() ) {
+
+      // If no conflict, call the function again to build upon that board
+      var result = findSolution(row + 1, n, board, validator, callback);
+      if ( result ) {
+        return result;
+      }
+    }
+
+    // Reset back to 0 so that the next iteration of the loop can move piece one space over
+    board.togglePiece(row, j);
+  }
+};
 
 
 window.findNRooksSolution = function(n) {
-  var solution = [];
-  for ( var i = 0; i < n; i++ ) {
-    solution[i] = [];
-    for ( var j = 0; j < n; j++ ) {
-      if (i === j) {
-        solution[i][j] = 1;
-      } else {
-        solution[i][j] = 0;
-      }
-    }
-  }
+  var board = new Board({n: n});
+  var solution = findSolution(0, n, board, 'hasAnyRooksConflicts', function() {
+    return _.map(board.rows(), function(row) {
+      return row.slice();
+    });
+  });
+
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-
-  
-  var solutions = [];
+  var solutionsCount = 0;
   var board = new Board({n: n});
 
-  var buildPossib = function (row, board) {
+  findSolution(0, n, board, 'hasAnyRooksConflicts', function() {
+    solutionsCount++;
+  });
 
-    // If we reach the end successfully, push the solution to solutions array
-    if (row === n) {
-      solutions.push(board);
-      return;
-    }
-
-    // Iterate over columns in the selected row
-    for ( var j = 0; j < n; j++ ) {
-
-      // Create a variant of the board with placement of piece at new col
-      // 'Row' variable is set at function invocation
-      board.get(row)[j] = 1;
-
-      if ( !board.hasAnyRooksConflicts() ) {
-
-        // If no conflict, call the function again to build upon that board
-        buildPossib(row + 1, board);
-
-      }
-
-      // Reset back to 0 so that the next iteration of the loop can move piece one space over
-      board.get(row)[j] = 0;
-    }
-  };
-
-  buildPossib(0, board);
-  var solutionCount = solutions.length;
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  console.log('Number of solutions for ' + n + ' rooks:', solutionsCount);
+  return solutionsCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  if (n === 0) {
-    return [];
-  } else if (n === 1) {
-    return [[1]];
-  } else {
-    // Create base board of all 0's
-    var solution = [];
-    for ( var i = 0; i < n; i++ ) {
-      solution[i] = [];
-      for ( var j = 0; j < n; j++ ) {
-        solution[i][j] = 0;
-      }
-    }
+  var board = new Board({n: n});
+  var solution = findSolution(0, n, board, 'hasAnyQueensConflicts', function() {
+    return _.map(board.rows(), function(row) {
+      return row.slice();
+    });
+  }) || board.rows();
 
-    // no solution for n = 2, n = 3
-    if (n === 2 || n === 3) {
-      return solution;
-    }
-
-    if (n === 8) {
-      return [
-        [0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 1],
-        [0, 0, 1, 0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0]
-      ];
-    }
-
-    var x = 0;
-    var y = 1;
-    solution[x][y] = 1;
-
-    // Knight maneuvers
-    while (x + 1 < n) {
-      x += 1;
-      if (y + 2 < n) {
-        y += 2;
-      } else {
-        y = 0;
-      }
-      solution[x][y] = 1;
-    }
-
-    console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-    return solution;
-  }
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  return solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutions = [];
+  var solutionsCount = 0;
   var board = new Board({n: n});
 
-  var buildPossib = function (row, board) {
+  findSolution(0, n, board, 'hasAnyQueensConflicts', function() {
+    solutionsCount++;
+  });
 
-    // If we reach the end successfully, push the solution to solutions array
-    if (row === n) {
-      solutions.push(board);
-      return;
-    }
-
-    // Iterate over columns in the selected row
-    for ( var j = 0; j < n; j++ ) {
-
-      // Create a variant of the board with placement of piece at new col
-      // 'Row' variable is set at function invocation
-      board.get(row)[j] = 1;
-
-      if ( !board.hasAnyQueensConflicts() ) {
-
-        // If no conflict, call the function again to build upon that board
-        buildPossib(row + 1, board);
-
-      }
-
-      // Reset back to 0 so that the next iteration of the loop can move piece one space over
-      board.get(row)[j] = 0;
-    }
-  };
-
-  buildPossib(0, board);
-  var solutionCount = solutions.length;
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+  console.log('Number of solutions for ' + n + ' queens:', solutionsCount);
+  return solutionsCount;
 };
